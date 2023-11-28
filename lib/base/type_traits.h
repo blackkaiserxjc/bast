9d07b032fff264ce9c0f33589b5a6d63d67d79fc
blackkaiserxjc <blackkaiserxjc@gmail.com>
2023-11-28 19:48:01 +0300

    feat: add hash module

--- lib/base/type_traits.h
+++ lib/base/type_traits.h
@@ -16,4 +16,17 @@ using return_type_of_t = typename std::result_of<F(Args...)>::type;
 
 #endif
 
+template <typename T, typename = void>
+struct is_char_array_helper : std::false_type
+{
+};
+
+template <std::size_t N>
+struct is_char_array_helper<char[N], std::void_t<decltype(sizeof(declval<char[N]>()))>> : std::true_type
+{
+};
+
+template <typename T>
+constexpr bool is_char_array_v = is_char_array_helper<T>::value;
+
 } // namespace bast
